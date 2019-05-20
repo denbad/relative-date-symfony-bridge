4e01897ce5765bdb7c47bda8581a1f240a09da7b
Your Name <you@example.com>
2019-05-20 23:15:37 +0300

    missing configuration bundle class

---
+++ src/DependencyInjection/RelativeDateConfiguration.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denbad\RelativeDateSymfonyBridge\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class RelativeDateConfiguration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('relative_date');
+
+        return $treeBuilder;
+    }
+}
