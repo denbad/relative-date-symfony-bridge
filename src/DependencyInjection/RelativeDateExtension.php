e4d11d161bf4f909114793943f912e75a7c9dc98
Your Name <you@example.com>
2019-05-25 17:38:36 +0300

    cleanup

--- src/DependencyInjection/RelativeDateExtension.php
+++ src/DependencyInjection/RelativeDateExtension.php
@@ -4,19 +4,42 @@ declare(strict_types=1);
 
 namespace Denbad\RelativeDateSymfonyBridge\DependencyInjection;
 
+use Denbad\RelativeDate\Middleware\TranslatesMiddleware;
+use Denbad\RelativeDateSymfonyBridge\Middleware\TranslatorAdapter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+use Denbad\RelativeDate\Formatter\CompositeFormatter;
 
 final class RelativeDateExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $this->configureDefaultFormats($config);
+        $this->setTranslatorAdapter();
+    }
+
+    private function configureDefaultFormats(ContainerBuilder $container, array $config): void
+    {
+        $container->getDefinition(CompositeFormatter::class)->setArgument(1, $config['default_strategy']);
+
+        foreach (array_keys($container->findTaggedServiceIds('date_formatter.format')) as $serviceId) {
+            $container->getDefinition($serviceId)->setArgument(0, $config['fallback_format']);
+        }
+    }
+
+    private function setTranslatorAdapter(ContainerBuilder $container): void
+    {
+        $container->getDefinition(TranslatesMiddleware::class)
+            ->setArgument(0, new Reference(TranslatorAdapter::class))
+        ;
     }
 }
--- src/RelativeDateBundle.php
+++ src/RelativeDateBundle.php
@@ -4,19 +4,6 @@ declare(strict_types=1);
 
 namespace Denbad\RelativeDateSymfonyBridge;
 
-use Denbad\RelativeDateSymfonyBridge\DependencyInjection\Compiler\ConfigureDefaultFormats;
-use Denbad\RelativeDateSymfonyBridge\DependencyInjection\Compiler\SetTranslatorAdapter;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class RelativeDateBundle extends Bundle
-{
-    public function build(ContainerBuilder $container): void
-    {
-        $container
-            ->addCompilerPass(new ConfigureDefaultFormats())
-            ->addCompilerPass(new SetTranslatorAdapter(), PassConfig::TYPE_OPTIMIZE)
-        ;
-    }
-}
+final class RelativeDateBundle extends Bundle {}
